4ff31440053261a280e97658fded970473e63273
Mostafa <mostafa.eg@thestock.sa>
2024-04-07 04:44:28 +0200

    start project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation/index.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    Navigation,
+  },
+}
+</script>
